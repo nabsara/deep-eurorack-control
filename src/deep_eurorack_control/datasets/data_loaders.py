fix nsynth dataloader normalization

src/deep_eurorack_control/datasets/data_loaders.py
@@ -43,5 +43,5 @@ def nsynth_data_loader(batch_size, data_dir=settings.DATA_DIR, audio_dir=setting
 
     # Define DataLoaders
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=num_threads)
-    valid_loader = torch.utils.data.DataLoader(valid_set, batch_size=batch_size, shuffle=True, num_workers=num_threads)
+    valid_loader = torch.utils.data.DataLoader(valid_set, batch_size=batch_size, shuffle=False, num_workers=num_threads)
     return train_loader, valid_loader

src/deep_eurorack_control/datasets/nsynth_dataset.py
@@ -1,27 +1,32 @@
 import os
 import json
 import scipy.io.wavfile
+import torch
+import librosa
 from torch.utils.data import Dataset
 import numpy as np
 
 
 class NSynthDataset(Dataset):
 
-    def __init__(self, data_dir, audio_dir, nsynth_json="nsynth_string.json", transform=None):
+    def __init__(self, data_dir, audio_dir, nsynth_json="nsynth_string.json", sampling_rate=16000, transform=None):
         super(NSynthDataset, self).__init__()
         with open(os.path.join(data_dir, nsynth_json), "r") as f:
             self.audio_labels = json.load(f)
         self.audio_name_list = list(self.audio_labels.keys())
         self.audio_dir = audio_dir
         self.transform = transform
+        self.sampling_rate = sampling_rate
 
     def __len__(self):
         return len(self.audio_labels)
 
     def __getitem__(self, index):
         audio_file = os.path.join(self.audio_dir, f"{self.audio_name_list[index]}.wav")
-        sr, audio = scipy.io.wavfile.read(audio_file)
-        audio = audio.astype(np.float32)
+        # sr, audio = scipy.io.wavfile.read(audio_file)
+        signal, _ = librosa.load(audio_file, self.sampling_rate)
+        audio = signal.astype(np.float32)
+        audio = torch.tensor(audio.copy()).float()
         if self.transform:
             audio = self.transform(audio)
         pitch = self.audio_labels[self.audio_name_list[index]]['pitch']